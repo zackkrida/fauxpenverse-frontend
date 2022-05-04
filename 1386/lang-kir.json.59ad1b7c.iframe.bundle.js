28a1e93483d60522ab0156f4a919e1be6808ea27
PR Closed action <openverse@wordpress.org>
2022-05-04 14:22:18 +0000

    Clean preview for PR 1386

--- 1386/lang-kir.json.59ad1b7c.iframe.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[117],{"./src/locales/kir.json":function(module){module.exports=JSON.parse("{}")}}]);
--- 1386/lang-so.json.c3567aed.iframe.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[160],{"./src/locales/so.json":function(module){module.exports=JSON.parse("{}")}}]);

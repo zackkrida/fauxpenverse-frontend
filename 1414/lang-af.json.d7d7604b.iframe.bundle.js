7e2c94ab492d662a7eb856ec707b2475ab8f770d
PR Closed action <openverse@wordpress.org>
2022-05-11 18:56:22 +0000

    Clean preview for PR 1414

--- 1414/lang-af.json.d7d7604b.iframe.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{"./src/locales/af.json":function(module){module.exports=JSON.parse("{}")}}]);
--- 1414/lang-oci.json.f6a213c1.iframe.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[143],{"./src/locales/oci.json":function(module){module.exports=JSON.parse("{}")}}]);
